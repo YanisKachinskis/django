5ca997d6af8862090eff5addd5717e4239fa79eb
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-09-27 19:04:17 +0300

    lesson03

--- myshop/mainapp/views.py
+++ myshop/mainapp/views.py
@@ -2,17 +2,12 @@ import json
 import os
 
 from django.shortcuts import render
-
+from mainapp.models import *
 from myshop.settings import BASE_DIR
 
+
 # Create your views here.
-links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+
 
 # похожие продукты на странице products.html
 with open(os.path.join(BASE_DIR, 'static', 'exmp.json'), 'r') as f:
@@ -20,28 +15,40 @@ with open(os.path.join(BASE_DIR, 'static', 'exmp.json'), 'r') as f:
     related_products = data['related_products']
 
 
-slogan = 'Новый уровень комфорта. Отличные характеристики.'
-
-
 def main(request):
+    slogan = 'Новый уровень комфорта. Отличные характеристики.'
+    products_list = Product.objects.all()[:3]
+
     content = {
+        'products_list': products_list,
         'slogan': slogan,
         'title': 'главная'
     }
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
+def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+    related_products_1 = Product.objects.filter(category_id=1)
+    related_products_2 = Product.objects.filter(category_id=2)
+    related_products_3 = Product.objects.filter(category_id=3)
     content = {
         'links_menu': links_menu,
         'title': 'продукты',
-        'related_products': related_products
+        'related_products_1': related_products_1,
+        'related_products_2': related_products_2,
+        'related_products_3': related_products_3,
+
     }
+    print(pk)
     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
+
+    shop_list = Shop.objects.all()
     content = {
-        'title': 'контакты'
+        'title': 'контакты',
+        'shop_list': shop_list
     }
     return render(request, 'mainapp/contact.html', content)
