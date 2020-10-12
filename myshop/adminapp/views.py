2a9e72b4423e27900ea3c5778f09a1dbb7396cb2
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-10-12 13:19:05 +0300

    Промежуточный коммит

---
+++ myshop/adminapp/views.py
@@ -0,0 +1,96 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, get_object_or_404
+
+from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def users(request):
+    title = 'админка/пользователи'
+
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+
+    content = {
+        'title': title,
+        'objects': users_list
+    }
+    return render(request, 'adminapp/users.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_delete(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_create(request):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = 'админка/категории'
+
+    categories_list = ProductCategory.objects.all()
+
+    content = {
+        'title': title,
+        'objects': categories_list,
+    }
+    return render(request, 'adminapp/categories.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_delete(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_create(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = 'продукты'
+
+    category_item = get_object_or_404(ProductCategory, pk=pk)
+    print(category_item)
+    products_list = Product.objects.filter(category=category_item).order_by('name')
+
+    content = {
+        'title': title,
+        'objects': products_list,
+        'category': category_item
+    }
+    return render(request, 'adminapp/products.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product(request, pk):
+    pass
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_delete(request, pk):
+    pass
