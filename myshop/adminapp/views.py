cbaa4c90124c876335b07dc34cdb31b81e8799f9
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-10-12 15:48:05 +0300

    Промежуточный коммит2

--- myshop/adminapp/views.py
+++ myshop/adminapp/views.py
@@ -1,13 +1,30 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
+from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm
 from authapp.models import ShopUser
 from mainapp.models import ProductCategory, Product
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserAdminEditForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserAdminEditForm()
+
+    content = {
+        'title': title,
+        'update_form': user_form,
+    }
+    return render(request, 'adminapp/user_update.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -25,17 +42,58 @@ def users(request):
 
 @user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        user_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        user_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {
+        'title': title,
+        'update_form': user_form,
+    }
+    return render(request, 'adminapp/user_update.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {
+        'title': title,
+        'user_to_delete': user,
+    }
+    return render(request, 'adminapp/user_delete.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
-    pass
+    title = 'категории/редактирование'
+
+    if request.method == 'POST':
+        category_form = ProductCategoryEditForm(request.POST, request.FILES)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect(reverse('admin:categories'))
+    else:
+        category_form = ProductCategoryEditForm()
+
+    content = {
+        'title': title,
+        'update_form': category_form,
+    }
+    return render(request, 'adminapp/category_update.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -85,6 +143,7 @@ def products(request, pk):
 @user_passes_test(lambda u: u.is_superuser)
 def product(request, pk):
     pass
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
