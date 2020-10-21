62b513f03d716021694228fa1f056dcb726850c4
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-10-21 23:48:50 +0300

    lesson09

--- myshop/authapp/views.py
+++ myshop/authapp/views.py
@@ -1,10 +1,38 @@
+from django.conf import settings
 from django.contrib import auth
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-
 from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from authapp.models import ShopUser
+
+
+def send_verification_email(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
+
+    subject = f'Активация пользователя {user.username}'
+
+    massage = f'Для подтверждения перейдите по ссылке:\n {settings.DOMAIN_NAME}{verify_link}'
+
+    return send_mail(subject, massage, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, activation_key):
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return render(request, 'authapp/verification.html')
+        else:
+            print(f'error: activation user: {email}')
+            return render(request, 'authapp/verification.html')
+    except Exception as e:
+        print(e)
+        return HttpResponseRedirect(reverse('main'))
 
 
 def login(request):
@@ -45,8 +73,13 @@ def register(request):
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
 
         if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
+            user = register_form.save()
+            if send_verification_email(user):
+                print('success')
+                return HttpResponseRedirect(reverse('auth:login'))
+            else:
+                print('error')
+                return HttpResponseRedirect(reverse('auth:login'))
 
     else:
         register_form = ShopUserRegisterForm()
@@ -57,7 +90,7 @@ def register(request):
 
     }
 
-    return render(request,'authapp/register.html', content)
+    return render(request, 'authapp/register.html', content)
 
 
 def edit(request):
